04957b75fe79eb50944c062bac9d4039b9be5ebd
alessandroamormino <amormino.alessandro97@gmail.com>
2023-05-11 15:43:05 +0200

    feat: add new migration for additional fields

---
+++ database/migrations/2023_05_11_133701_add_date_to_trains.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('trains', function (Blueprint $table) {
+            $table->date('data_partenza')->after('stazione_arrivo');
+            $table->date('data_arrivo')->after('data_partenza');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('trains', function (Blueprint $table) {
+            $table->dropColumn('data_partenza');
+            $table->dropColumn('data_arrivo');
+        });
+    }
+};
