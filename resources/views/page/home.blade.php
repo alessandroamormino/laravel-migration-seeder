ef690c87016dbc338c3fa0d1482736966a8fe22e
alessandroamormino <amormino.alessandro97@gmail.com>
2023-05-12 15:35:42 +0200

    fix: adapt new columns name in table

--- resources/views/page/home.blade.php
+++ resources/views/page/home.blade.php
@@ -8,7 +8,8 @@
     <table class="table table-hover">
       <thead>
         <tr>
-          <th scope="col">Azienda</th>
+          {{-- METODO CON FAKER --}}
+          {{-- <th scope="col">Azienda</th>
           <th scope="col">Stazione di Partenza</th>
           <th scope="col">Stazione di Arrivo</th>   
           <th scope="col">Data di Partenza</th>
@@ -18,13 +19,25 @@
           <th scope="col">Codice Treno</th>
           <th scope="col">Numero Carrozze</th>
           <th scope="col">In Orario</th>
-          <th scope="col">Cancellato</th>   
+          <th scope="col">Cancellato</th>    --}}
+
+          {{-- METODO CON FILE CSV --}}
+          <th scope="col">Azienda</th>
+          <th scope="col">Stazione di Partenza</th>
+          <th scope="col">Stazione di Arrivo</th>   
+          <th scope="col">Data di Partenza</th>
+          <th scope="col">Data di Arrivo</th>
+          <th scope="col">Codice Treno</th>
+          <th scope="col">Numero Carrozze</th>
+          <th scope="col">In Orario</th>
+          <th scope="col">Cancellato</th> 
         </tr>
       </thead>
       <tbody>
         @foreach($trains as $singleTrain)
           <tr>
-            <td>{{$singleTrain->azienda}}</td>
+            {{-- METODO CON FAKER --}}
+            {{-- <td>{{$singleTrain->azienda}}</td>
             <td>{{$singleTrain->stazione_partenza}}</td>
             <td>{{$singleTrain->stazione_arrivo}}</td>
             <td>{{$singleTrain->data_partenza}}</td>
@@ -34,7 +47,18 @@
             <td>{{$singleTrain->cod_treno}}</td>
             <td>{{$singleTrain->n_carrozze}}</td>
             <td>{{$singleTrain->in_orario ? 'Si' : 'No'}}</td>
-            <td>{{$singleTrain->cancellato ? 'Si' : 'No'}}</td>
+            <td>{{$singleTrain->cancellato ? 'Si' : 'No'}}</td> --}}
+
+            {{-- METODO CON FILE CSV --}}
+            <td>{{$singleTrain->company}}</td>
+            <td>{{$singleTrain->departure_station}}</td>
+            <td>{{$singleTrain->arrival_station}}</td>
+            <td>{{$singleTrain->departure_time}}</td>
+            <td>{{$singleTrain->arrival_time}}</td>
+            <td>{{$singleTrain->train_code}}</td>
+            <td>{{$singleTrain->wagons_number}}</td>
+            <td>{{$singleTrain->on_time ? 'Si' : 'No'}}</td>
+            <td>{{$singleTrain->cancelled ? 'Si' : 'No'}}</td>
           </tr>
         @endforeach
       </tbody>
